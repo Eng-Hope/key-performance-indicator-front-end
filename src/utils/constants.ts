18187ba4609a31e7232ce0d14310b2c4c7c1b010
Hope Richard Maleko <hoperichardmaleko@gmail.com>
2025-02-11 11:29:56 +0300

    fix side bar

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -14,7 +14,6 @@ export const sideBarData = [
     menu: [
       {
         title: "Dashboard",
-        url: "/home",
         icon: LayoutDashboard,
         submenu: [
           { title: "dashboard", url: "/home" },
